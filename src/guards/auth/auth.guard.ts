e7e96c7253d97cb5d60d363050dad2e7e57f99c5
Shubham Saini <shubhamsaini12594@gmail.com>
2026-03-13 16:27:04 +0530

    Learn the Authentication and Authorization with Guard

---
+++ src/guards/auth/auth.guard.ts
@@ -0,0 +1,20 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const authHeader = request.headers['authorization'];
+    if (!authHeader) {
+      return false;
+    } 
+    return authHeader === 'Bearer my-secret-token';
+  }
+}
