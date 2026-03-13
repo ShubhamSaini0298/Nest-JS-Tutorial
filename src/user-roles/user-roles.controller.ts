86412cdc044f99a0cb708648228f4d081236ac34
Shubham Saini <shubhamsaini12594@gmail.com>
2026-03-13 17:39:29 +0530

    Role based acces the routes using guards and protect the routes

---
+++ src/user-roles/user-roles.controller.ts
@@ -0,0 +1,20 @@
+import { Get, Controller, UseGuards } from '@nestjs/common';
+import { Roles } from 'src/guards/roles/roles.decorator';
+import { Role } from 'src/guards/roles/roles.enums';
+import { RolesGuard } from 'src/guards/roles/roles.guard';
+
+@Controller('user-roles')
+export class UserRolesController {
+
+    @Get('admin-data')
+    @UseGuards(RolesGuard)
+    @Roles(Role.Admin)
+    getAdminData() {
+        return {message : 'Only admin can acccess this data'}
+    }
+
+    @Get('user-data')
+    getUserData() {
+        return {message : 'Anyone can access this data'}
+    }
+}
