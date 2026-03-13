6b9b814b7f78a3a574edfddf99d2c0756c943718
Shubham Saini <shubhamsaini12594@gmail.com>
2026-03-13 15:43:13 +0530

    Implementation of custom pipe like to make upper case

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
@@ -7,8 +8,8 @@ async function bootstrap() {
 
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
+      whitelist: true,  // This remove the properties that are not defined in the DTO
+      forbidNonWhitelisted: true, //This throws an error if extra fields are sent instead of silently removing them.
     }),
   );
 
