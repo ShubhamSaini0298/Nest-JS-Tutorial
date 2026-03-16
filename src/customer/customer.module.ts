b0a8dbbc9c058714a10d6eefbfba057f25fdd912
Shubham Saini <shubhamsaini12594@gmail.com>
2026-03-16 16:02:22 +0530

    Exception and filter in nest js

--- src/customer/customer.module.ts
+++ src/customer/customer.module.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { CustomerService } from './customer.service';
 import { CustomerController } from './customer.controller';
